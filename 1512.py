0cb32688b19ab68835de5e35feefa6f56ddad55e
AruzhanBazarbai <aruzhanart2003@mail.ru>
2021-02-02 19:03:02 +0600

    third commit

--- 1512.py
+++ 1512.py
@@ -1,5 +1,14 @@
 arr=input().split()
 cnt=0
-#print(arr)
 
-i=0
+
+for i in range (0,len(arr)):
+    b=arr[i]
+    x=i
+    for j in range (x+1,len(arr)):
+        if b==arr[j] and x<j:
+            cnt+=1
+
+print(cnt)
+   
+
